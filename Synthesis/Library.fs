f291b6e188330d66dd0fbda82ad5b195ecb63a7a
g18j2582 <g18j2582@HUG137>
2020-03-11 20:20:06 +0200

    initial commit

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -1,34 +1,58 @@
 ﻿module Synthesis
 
-let abelar _ =
-    failwith "Not implemented"
+let abelar input = input > 12 && input < 3097 && input % 12 = 0 
+    //failwith "Not implemented"
 
-let area _ _ =
-    failwith "Not implemented"
+let area length height = 
+    match length >= 0.0 && height >= 0.0 with
+    |true -> length / 2.0 * height
+    |false -> failwith "Noooo"
+    
 
-let zollo _ =
-    failwith "Not implemented"
+let zollo input =
+    match input >= 0 with
+    |true -> input * 2
+    |false -> input * -1 
+    //failwith "Not implemented"
 
-let min _ _ =
-    failwith "Not implemented"
+let min num1 num2 =
+    match num1 > num2 with 
+    |true -> num2
+    |false -> num1
 
-let max _ _ =
-    failwith "Not implemented"
+let max num1 num2 =
+    match num1 > num2 with
+    |true -> num1
+    |false -> num2
 
-let ofTime _ _ _ =
-    failwith "Not implemented"
+let ofTime h m s = h * 3600 + m * 60 + s
 
-let toTime _ =
-    failwith "Not implemented"
+let toTime input =  
+    match input < 0 with 
+    |true -> (0,0,0)
+    |false -> 
+    let h = input / 3600
+    let m = input % 3600 / 60
+    let s = input % 3600 % 60
+    (h, m, s)
+    
 
-let digits _ =
-    failwith "Not implemented"
+let digits input =
+    let rec count a acc= 
+        match  (a < 10 && a > -10) with
+        |true -> acc
+        |false -> count (a / 10) (acc + 1)
+    count input 1
+   
 
 let minmax _ =
     failwith "Not implemented"
 
-let isLeap _ =
-    failwith "Not implemented"
+let isLeap year = match (year >= 1582)  with
+                  |true -> match  (not(year % 100 = 0)|| year % 400 = 0) && (year % 4 = 0) with
+                           |true -> true
+                           |false -> false
+                  |false -> failwith "not leap year"
 
 let month _ =
     failwith "Not implemented"
